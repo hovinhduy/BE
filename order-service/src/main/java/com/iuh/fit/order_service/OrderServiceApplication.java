e3ed6d625c8dfb239969476a8783ed1cd4261767
hovinhduy <hovinhduy0712@gmail.com>
2025-04-23 19:35:31 +0700

    Cập nhật cấu hình gateway-service để sử dụng đường dẫn API cho inventory và order, thêm @EnableDiscoveryClient vào InventoryServiceApplication và OrderServiceApplication, và điều chỉnh URL của inventory-service trong order-service.

--- order-service/src/main/java/com/iuh/fit/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/iuh/fit/order_service/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.iuh.fit.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 @OpenAPIDefinition(
     info = @Info(
         title = "Order Service API",
