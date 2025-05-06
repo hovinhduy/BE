3e5a91a197520440235a2c3eba6b84f0cbdacbc0
Le Ba Hau <haubtm699@gmail.com>
2025-05-06 17:59:29 +0700

    fix: getter and setter prouductDTO

--- product-service/src/main/java/com/ktpm/productService/dto/ProductDTO.java
+++ product-service/src/main/java/com/ktpm/productService/dto/ProductDTO.java
@@ -11,4 +11,60 @@ public class ProductDTO {
     private int quantity;
     private Long categoryId;
     private Long manufactureId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getDetailDesc() {
+        return detailDesc;
+    }
+
+    public void setDetailDesc(String detailDesc) {
+        this.detailDesc = detailDesc;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getManufactureId() {
+        return manufactureId;
+    }
+
+    public void setManufactureId(Long manufactureId) {
+        this.manufactureId = manufactureId;
+    }
 }
